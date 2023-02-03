6c445d548d953b2674974bd893f09c4632357279
alvaroCoelho <alvarofreitascoelho@gmail.com>
2023-02-03 10:22:48 -0300

    create navigation

--- app/src/main/java/com/marvelcomic/ui/screens/StartScreen.kt
+++ app/src/main/java/com/marvelcomic/ui/screens/StartScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,14 +13,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.marvelcomic.R
 
-@Preview
+
 @Composable
-fun HomeScreen(){
+fun StartScreen(navHostController : NavHostController){
 
     Column(
         modifier = Modifier
@@ -47,7 +46,9 @@ fun HomeScreen(){
                 backgroundColor = colorResource(id = R.color.red_marvel),
                 contentColor = Color.White,
                 ),
-                onClick = { /*TODO*/ }) {
+                onClick = {
+                    navHostController.navigate(Destinations.ChoiceCharacterScreen.route)
+                }) {
                 Text(text = stringResource(id = R.string.start),
                 fontSize = 24.sp)
 
